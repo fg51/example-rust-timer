4acd36294dea491bf8a94a129ccb50159a6b5b55
kflange <zknsftk1msrk@gmail.com>
2020-11-30 16:30:37 +0900

    Added: timer::interval

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use tokio::time::Instant;
+use tokio::time::Duration;
+use tokio::time::interval as time_interval;
+
 async fn hello() -> String {
     "hello, async fn".to_string()
 }
 
+
 #[tokio::main]
 async fn main() {
-    let greeting = hello().await;
-    println!("{}", greeting);
+    let start_at = Instant::now();
+    let mut interval = time_interval(Duration::from_millis(100));
+
+    for _ in 0..5 {
+        interval.tick().await;
+        println!("{:04} [msec]", start_at.elapsed().as_millis());
+        let greeting = hello().await;
+        println!("{}", greeting);
+    }
 }
